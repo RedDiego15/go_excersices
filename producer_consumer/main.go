56b4e59c567c85b04441e0a82b77c49827cc25eb
diego-27ro@outlook.com <diegorojas@Diegos-Laptop-2.local>
2026-03-08 01:14:45 -0500

    producer

---
+++ producer_consumer/main.go
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Given is a producer-consumer scenario, where a producer reads in
+// tweets from a mockstream and a consumer is processing the
+// data. Your task is to change the code so that the producer as well
+// as the consumer can run concurrently
+//
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func producer(stream Stream, tweets chan<- *Tweet) {
+	defer close(tweets)
+	for {
+		tweet, err := stream.Next()
+		if err == ErrEOF {
+			return
+		}
+		tweets <- tweet
+	}
+}
+
+func consumer(tweets <-chan *Tweet) {
+	for t := range tweets {
+		if t.IsTalkingAboutGo() {
+			fmt.Println(t.Username, "\ttweets about golang")
+		} else {
+			fmt.Println(t.Username, "\tdoes not tweet about golang")
+		}
+	}
+}
+
+func main() {
+	start := time.Now()
+	stream := GetMockStream()
+
+	tweets := make(chan *Tweet)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		producer(stream, tweets)
+	}()
+
+	go func() {
+		defer wg.Done()
+		consumer(tweets)
+	}()
+
+	wg.Wait()
+	fmt.Printf("Process took %s\n", time.Since(start))
+}
